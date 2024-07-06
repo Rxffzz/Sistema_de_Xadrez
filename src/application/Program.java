5310f1bf856bef84426f583d9cd84a32bf3d9028
Rxffzz <srafael0406@gmail.com>
2024-07-06 18:52:31 -0300

    First Class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
-public class src {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args){
+
+
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
